8631c74623997b8596c271202329e76b240e650d
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-05-16 15:45:19 -0300

    Update adv.py

--- adv.py
+++ adv.py
@@ -122,7 +122,7 @@ label {
 
     background: #0b0b0b !important;
 
-    border: 1px solid rgba(0,255,208,0.45) !important;
+    border: 1px solid #00aaff !important;
 
     border-radius: 5px !important;
 
